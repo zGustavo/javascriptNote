9726c172a7be963d0f379e70accf72debddb0195
Gustavo Dias <dev.gustavo@hotmail.com>
2022-05-30 23:21:30 -0300

    inicio javascript_note

---
+++ config/database.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+mongoose.connect('mongodb://localhost/javascriptNote', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then( () => console.log('Connection successful'))
+.catch((err) => console.log(err));
